b3c3a5fa85c0ec4486c7c86f2019f23fb432b32c
Иванов Иван <k0t9i@yandex.ru>
2024-08-14 17:48:19 +0200

    fix: escape json strings

--- L2BotDll/Serializers/JsonSerializer.h
+++ L2BotDll/Serializers/JsonSerializer.h
@@ -24,7 +24,7 @@ public:
 			}
 			else
 			{
-				result += L"\"" + it->value + L"\"";
+				result += L"\"" + EscapeJson(it->value) + L"\"";
 			}
 			if (std::next(it) != nodes.end())
 			{
@@ -35,5 +35,31 @@ public:
 		result += isArray ? L"]" : L"}";
 
 		return result;
+	}
+
+private:
+	const std::wstring EscapeJson(const std::wstring& input) const
+	{
+		std::wstringstream o;
+		for (auto c = input.cbegin(); c != input.cend(); c++) {
+			switch (*c) {
+			case '"': o << "\\\""; break;
+			case '\\': o << "\\\\"; break;
+			case '\b': o << "\\b"; break;
+			case '\f': o << "\\f"; break;
+			case '\n': o << "\\n"; break;
+			case '\r': o << "\\r"; break;
+			case '\t': o << "\\t"; break;
+			default:
+				if ('\x00' <= *c && *c <= '\x1f') {
+					o << "\\u"
+						<< std::hex << std::setw(4) << std::setfill(L'0') << static_cast<int>(*c);
+				}
+				else {
+					o << *c;
+				}
+			}
+		}
+		return o.str();
 	}
 };
